764c0666e6770be379471b830cb66d8ef8c3741e
Parham Alvani <parham.alvani@gmail.com>
2018-11-06 00:09:35 +0330

    Better syntax.

--- ps/fork.c
+++ ps/fork.c
@@ -7,39 +7,37 @@
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
-*/
+ */
 /*
  * Copyright (c) 2016 Parham Alvani.
-*/
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 
-int say_hello(const char *name)
-{
-	int i;
+int say_hello(const char *name) {
+  int i;
 
-	for (i = 0; i < 4; i++) {
-		printf("hello %s\n", name);
+  for (i = 0; i < 4; i++) {
+    printf("hello %s\n", name);
   }
 
   return 0;
 }
 
-int main(int argc, char *argv[])
-{
-	pid_t pid;
+int main(int argc, char *argv[]) {
+  pid_t pid;
 
-	pid = fork();
+  pid = fork();
 
-	if (pid < 0) {
-		perror("fork()");
-	} else if (pid == 0) {
-		say_hello("child");
-		exit(0);
-	} else {
-		sleep(1);
-		say_hello("parent");
-	}
+  if (pid < 0) {
+    perror("fork()");
+  } else if (pid == 0) {
+    say_hello("child");
+    exit(0);
+  } else {
+    sleep(1);
+    say_hello("parent");
+  }
 }
