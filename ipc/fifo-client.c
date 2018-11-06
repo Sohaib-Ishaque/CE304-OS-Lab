65bd45fe4e69e843a5684128f2f3b096dc761507
Parham Alvani <parham.alvani@gmail.com>
2018-11-06 06:22:08 +0330

    FIFO client is ready

--- ipc/fifo-client.c
+++ ipc/fifo-client.c
@@ -1,18 +1,36 @@
+/*
+ * In The Name of God
+ * =======================================
+ * [] File Name : fifo-client.c
+ *
+ * [] Creation Date : 06-11-2018
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+ */
+/*
+ * Copyright (c)  2018 Parham Alvani.
+ */
+
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-	int n;
-	FILE *server;
+/*
+ * This program opens a pipe that is builded by fifo-server.c and writes user input
+ * to it.
+ */
 
-	server = fopen("/tmp/parham_fifo_sv", "w");
+int main(int argc, char *argv[]) {
+  int n;
+  FILE *server;
 
-	do {
-		scanf("%d", &n);
-		printf("You entered: %d\n", n);
-		fprintf(server, "%d\n", n);
-		fflush(server);
-	} while (n != 0);
+  server = fopen("/tmp/parham_fifo_sv", "w"); // opens fifo to write
+
+  do { // reads user input until it is not equal to zero
+    scanf("%d", &n);
+    printf("You entered: %d\n", n);
+    fprintf(server, "%d\n", n); // writes user input to fifo
+    fflush(server);
+  } while (n != 0);
 }
