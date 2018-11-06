b1e1b1dcb66901535918486ddf43784a13d72bcf
Parham Alvani <parham.alvani@gmail.com>
2018-11-06 06:19:06 +0330

    FIFO server is ready.

--- ipc/fifo-server.c
+++ ipc/fifo-server.c
@@ -1,18 +1,36 @@
+/*
+ * In The Name of God
+ * =======================================
+ * [] File Name : fifo-server.c
+ *
+ * [] Creation Date : 06-11-2018
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+ */
+/*
+ * Copyright (c)  2018 Parham Alvani.
+ */
+
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-	int n;
-	FILE *server;
+/*
+ * This program builds a fifo in /tmp then open it and waits for data on it.
+ * When data available this program reads it as a number n and calculates n * (n + 1) / 2
+ */
 
-	mkfifo("/tmp/parham_fifo_sv", S_IRUSR | S_IWUSR | S_IWGRP);
-	server = fopen("/tmp/parham_fifo_sv", "r");
+int main(int argc, char *argv[]) {
+  int n;
+  FILE *server;
 
-	for (;;) {
-		printf("Waiting for client data\n");
-		fscanf(server, "%d", &n);
-		printf("You answer is: %d\n", (n * (n + 1)) / 2);
-	}
+  mkfifo("/tmp/parham_fifo_sv", S_IRUSR | S_IWUSR | S_IWGRP);
+  server = fopen("/tmp/parham_fifo_sv", "r");
+
+  for (;;) {
+    printf("Waiting for client data\n");
+    fscanf(server, "%d", &n); // blocking operation
+    printf("You answer is: %d\n", (n * (n + 1)) / 2);
+  }
 }
