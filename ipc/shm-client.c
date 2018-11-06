a6b066886eb9741cf36ee99c8f9a725e0cf5ed1b
Parham Alvani <parham.alvani@gmail.com>
2018-11-06 06:40:02 +0330

    Shared memory samples are ready.

--- ipc/shm-client.c
+++ ipc/shm-client.c
@@ -1,36 +1,49 @@
+/*
+ * In The Name of God
+ * =======================================
+ * [] File Name : shm-client.c
+ *
+ * [] Creation Date : 06-11-2018
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+ */
+/*
+ * Copyright (c)  2018 Parham Alvani.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-int main()
-{
-	int shmid;
-	key_t key = 0x01020304;
-	void *addr;
+int main(int argc, const char *argv[]) {
+  int shmid;
+  key_t key = 0x01020304;
+  void *addr;
 
-	shmid = shmget(key, 4096, 0);
-	if (shmid < 0) {
-		fprintf(stderr, "failed to create shm segment\n");
-		perror("shmget");
-		return -1;
-	}
+  shmid = shmget(key, 4096, 0);
+  if (shmid < 0) {
+    fprintf(stderr, "failed to create shm segment\n");
+    perror("shmget");
+    return -1;
+  }
 
-	printf("found %d\n", shmid);
+  printf("found %d\n", shmid);
 
-	addr = shmat(shmid, NULL, 0);
-	if ((long long) addr == -1) {
-		fprintf(stderr, "failed to attach\n");
-		perror("shmat");
-		return -1;
-	}
+  addr = shmat(shmid, NULL, 0);
+  if ((long long) addr == -1) {
+    fprintf(stderr, "failed to attach\n");
+    perror("shmat");
+    return -1;
+  }
 
-	printf("got %p\n", addr);
+  printf("got %p\n", addr);
 
-	char *data = addr;
+  char *data = addr;
 
-	printf("Data %s\n", data);
+  printf("Data %s\n", data);
 
-	return 0;
+  return 0;
 }
--- ipc/shmget.c
+++ ipc/shmget.c
@@ -1,22 +1,35 @@
+/*
+ * In The Name of God
+ * =======================================
+ * [] File Name : shmget.c
+ *
+ * [] Creation Date : 06-11-2018
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+ */
+/*
+ * Copyright (c)  2018 Parham Alvani.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-int main()
-{
-	int shmid;
-	key_t key = 0x01020304;
+int main(int argc, const char *argv[]) {
+  int shmid;
+  key_t key = 0x01020304;
 
-	// create 4096 bytes of shared memory
-	shmid = shmget(key, 4096, IPC_CREAT);
-	if (shmid < 0) {
-		fprintf(stderr, "failed to create shm segment\n");
-		perror("shmget");
-		return -1;
-	}
+  // create 4096 bytes of shared memory
+  shmid = shmget(key, 4096, IPC_CREAT);
+  if (shmid < 0) {
+    fprintf(stderr, "failed to create shm segment\n");
+    perror("shmget");
+    return -1;
+  }
 
-	printf("created %d\n", shmid);
-	return 0;
+  printf("created %d\n", shmid);
+  return 0;
 }
