48344b7d8ad72c41cbf725661e6383533a7f1ee0
Parham Alvani <parham.alvani@gmail.com>
2018-11-20 00:04:29 +0330

    Better retval sample.

--- thread/retval.c
+++ thread/retval.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// student represents a collegian
 struct student {
 	int number;
 	char* name;
 };
 
+// print_student is a function that is called by pthread within a new thread.
+// this function prints student information.
 void *print_student(void *input) {
 	struct student *std = (struct student *)input;
 	printf("Hello %s! Your student ID is %d\n", std->name, std->number);
@@ -15,18 +18,16 @@ void *print_student(void *input) {
 }
 
 int main() {
-	struct student mohammad;
-	mohammad.number = 9231058;
-	mohammad.name = "Mohammad";
+  // defines a student
+	struct student parham;
+	parham.number = 9231058;
+	parham.name = "Parham";
 
 	pthread_t thread;
 
-	pthread_create(&thread, NULL, print_student, (void *)&mohammad);
-
-
+	pthread_create(&thread, NULL, print_student, (void *)&parham);
 
 	void *retval;
-
 
 	pthread_join(thread, &retval);
 	printf("Exit: %ld\n", (long)retval);
