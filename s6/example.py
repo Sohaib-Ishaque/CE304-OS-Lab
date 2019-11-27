68d025532719d8fb070d2170dfc5b79da6831a9f
Parham Alvani <parham.alvani@gmail.com>
2019-11-27 10:29:25 +0330

    feat: Better Python Threading Example

--- s6/example.py
+++ s6/example.py
@@ -11,11 +11,14 @@ import threading
 
 lock = threading.Lock()
 
+counter = 0
 
 def say_hello(n):
+    global counter
     for i in range(0, 10):
         lock.acquire()
-        print(f'Thread {n}: {i}')
+        counter += 1
+        print(f'Thread {n}: {i} -> {counter}')
         lock.release()
 
 
